ea0aebf632a1eb9c750b3839db33bcecf162639f
donny <dramier17@gmail.com>
2018-08-28 18:54:27 -0500

    chooseRandom working, export > npm test indexArray not defined bug

--- src/lib.js
+++ src/lib.js
@@ -1,10 +1,37 @@
 const chooseRandom = (array, numItems) => {
   // TODO check that array length is greater than 1
-  const randomIndices = Array(numItems) // TODO ensure numItems is within the range 1 to the length of the array
-    .fill() // Don't change this line. Just fills the array we're about to reduce.
-    .reduce((res, next) => {
-      // reduce the array to contain numItems # of unique index values
-    })
-  // TODO return a new array filtering the passed in array for only the
-  // indices contained in the randomIndices array
+  if (array.length <= 2){
+    return array
+  }
+  if (numItems > array.length){
+    numItems = Math.floor(Math.random() * array.length)
+  }
+  
+  indexArray = []
+  //console.log(indexArray)
+  //Fill indexArray with random indices
+  while (indexArray.length != numItems){
+    randInt = Math.floor(Math.random()*array.length)
+    if (indexArray.length === 0){
+      indexArray.push(randInt)
+    }
+    inArray = false
+    for (let i = 0; i <indexArray.length; i++){
+      if (indexArray[i]===randInt){
+        inArray = true
+      }
+    }
+    if (inArray == false){
+      indexArray.push(randInt)
+    }
+  }
+//return the new array with the random values from indices indicated in indexArray
+  filteredArray = []
+  for (let i = 0; i< indexArray.length;i++){
+    filteredArray.push(array[indexArray[i]])
+  }
+  return filteredArray
 }
+
+ray = [1,2,3,4,5]
+console.log(chooseRandom(ray,22))
