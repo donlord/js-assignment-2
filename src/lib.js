82e5c4f5e14a1bcccf8f37729ba02614913d030c
donny <dramier17@gmail.com>
2018-08-29 10:00:07 -0500

    added prettify to make code look nice

--- src/lib.js
+++ src/lib.js
@@ -1,34 +1,34 @@
 export const chooseRandom = (array, numItems) => {
-  if (array === undefined){
+  if (array === undefined) {
     return []
   }
-  if (array.length <= 2){
+  if (array.length <= 2) {
     return array
   }
-  if (numItems > array.length || numItems === undefined){
+  if (numItems > array.length || numItems === undefined) {
     numItems = Math.floor(Math.random() * array.length)
   }
   let indexArray = []
-  //Fill indexArray with random indices
-  while (indexArray.length != numItems){
-    let randInt = Math.floor(Math.random()*array.length)
-    if (indexArray.length === 0){
+  // Fill indexArray with random indices
+  while (indexArray.length != numItems) {
+    let randInt = Math.floor(Math.random() * array.length)
+    if (indexArray.length === 0) {
       indexArray.push(randInt)
     }
     let inArray = false
-    //check to see if randomInt is in the indexed array, if so continue else add it to the indexArray
-    for (let i = 0; i < indexArray.length; i++){
-      if (indexArray[i] === randInt){
+    // check to see if randomInt is in the indexed array, if so continue else add it to the indexArray
+    for (let i = 0; i < indexArray.length; i++) {
+      if (indexArray[i] === randInt) {
         inArray = true
       }
     }
-    if (inArray == false){
+    if (inArray == false) {
       indexArray.push(randInt)
     }
   }
-//return the new array with the random values from indices indicated in indexArray
+  // return the new array with the random values from indices indicated in indexArray
   let filteredArray = []
-  for (let i = 0; i < indexArray.length; i++){
+  for (let i = 0; i < indexArray.length; i++) {
     filteredArray.push(array[indexArray[i]])
   }
   return filteredArray
